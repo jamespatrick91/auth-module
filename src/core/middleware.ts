49c0f4a432b36bf24bac07a4b62ccadc730312cd
James Patrick <jamesapatrick1991@gmail.com>
2021-05-17 15:30:50 +1000

    Trialing Possible Solutions

--- src/core/middleware.ts
+++ src/core/middleware.ts
@@ -1,51 +1,69 @@
 import { routeOption, getMatchedComponents, normalizePath } from '../utils'
 
-export default async function authMiddleware (ctx) {
-  // Disable middleware if options: { auth: false } is set on the route
-  if (routeOption(ctx.route, 'auth', false)) {
-    return
-  }
+export default async function authMiddleware(ctx) {
+	// Disable middleware if options: { auth: false } is set on the route
+	if (routeOption(ctx.route, 'auth', false)) {
+		return
+	}
 
-  // Disable middleware if no route was matched to allow 404/error page
-  const matches = []
-  const Components = getMatchedComponents(ctx.route, matches)
-  if (!Components.length) {
-    return
-  }
+	// Disable middleware if no route was matched to allow 404/error page
+	const matches = []
+	const Components = getMatchedComponents(ctx.route, matches)
+	if (!Components.length) {
+		return
+	}
 
-  const { login, callback } = ctx.$auth.options.redirect
-  const pageIsInGuestMode = routeOption(ctx.route, 'auth', 'guest')
-  const insidePage = page => normalizePath(ctx.route.path) === normalizePath(page)
+	const { login, callback } = ctx.$auth.options.redirect
+	const pageIsInGuestMode = routeOption(ctx.route, 'auth', 'guest')
+	const insidePage = page => normalizePath(ctx.route.path) === normalizePath(page)
 
-  if (ctx.$auth.$state.loggedIn) {
-    // -- Authorized --
-    if (!login || insidePage(login) || pageIsInGuestMode) {
-		ctx.$auth.redirect('home')
-    }
+	if (
+		!ctx.$auth.$state.loggedIn &&
+		ctx != null &&
+		ctx.$auth != null &&
+		ctx.$auth.strategies != null &&
+		ctx.$auth.strategies.cookie != null &&
+		ctx.$auth.strategies.cookie.token != null &&
+		ctx.$auth.strategies.cookie.token.$storage != null &&
+		ctx.$auth.strategies.cookie.token.$storage._state != null &&
+		ctx.$auth.strategies.cookie.token.$storage._state['_token.cookie'] != null
+	) {
+		try {
+			await ctx.$axios.post('/cookie_check', { jwt_token: ctx.$auth.strategies.cookie.token.$storage._state['_token.cookie'].replace('Bearer ', '') }, { useCredentials: true })
+		} catch (e) {
+			console.log(e)
+		}
+	}
 
-    // Perform scheme checks.
-    const { tokenExpired, refreshTokenExpired, isRefreshable } = ctx.$auth.check(true)
+	if (ctx.$auth.$state.loggedIn) {
+		// -- Authorized --
+		if (!login || insidePage(login) || pageIsInGuestMode) {
+			ctx.$auth.redirect('home')
+		}
 
-    // Refresh token has expired. There is no way to refresh. Force reset.
-    if (refreshTokenExpired) {
-      ctx.$auth.reset()
-    } else if (tokenExpired) {
-      // Token has expired. Check if refresh token is available.
-      if (isRefreshable) {
-        // Refresh token is available. Attempt refresh.
-        await ctx.$auth.refreshTokens()
-      } else {
-        // Refresh token is not available. Force reset.
-        ctx.$auth.reset()
-      }
-    }
+		// Perform scheme checks.
+		const { tokenExpired, refreshTokenExpired, isRefreshable } = ctx.$auth.check(true)
 
-    // -- Guest --
-    // (Those passing `callback` at runtime need to mark their callback component
-    // with `auth: false` to avoid an unnecessary redirect from callback to login)
-  } else if (!pageIsInGuestMode && (!callback || !insidePage(callback))) {
-    console.log('test 3')
-	debugger
-	ctx.$auth.redirect('login')
-  }
+		// Refresh token has expired. There is no way to refresh. Force reset.
+		if (refreshTokenExpired) {
+			ctx.$auth.reset()
+		} else if (tokenExpired) {
+			// Token has expired. Check if refresh token is available.
+			if (isRefreshable) {
+				// Refresh token is available. Attempt refresh.
+				await ctx.$auth.refreshTokens()
+			} else {
+				// Refresh token is not available. Force reset.
+				ctx.$auth.reset()
+			}
+		}
+
+		// -- Guest --
+		// (Those passing `callback` at runtime need to mark their callback component
+		// with `auth: false` to avoid an unnecessary redirect from callback to login)
+	} else if (!pageIsInGuestMode && (!callback || !insidePage(callback))) {
+		console.log('test 3')
+		debugger
+		ctx.$auth.redirect('login')
+	}
 }
