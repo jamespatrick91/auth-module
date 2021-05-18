a7801d075d9c1453825cbc302e297e46011119f9
James Patrick <jamesapatrick1991@gmail.com>
2021-05-18 19:35:20 +1000

    Rolling Changes

--- dist/core/middleware.js
+++ dist/core/middleware.js
@@ -22,25 +22,14 @@ export default async function authMiddleware(ctx) {
         ctx.$auth.strategies.cookie.token.$storage != null &&
         ctx.$auth.strategies.cookie.token.$storage._state != null &&
         ctx.$auth.strategies.cookie.token.$storage._state['_token.cookie'] != null) {
-        try {
-            let auth_token = ctx.$auth.strategies.cookie.token.$storage._state['_token.cookie'];
-            if (~auth_token.indexOf('Bearer '))
-                auth_token = auth_token.replace('Bearer ', '');
-            await ctx.$axios.get('/debugger', { params: {
-                    message: 'commencing'
-                } });
-            await ctx.$axios.post('/cookie_check', { jwt_token: auth_token }, { useCredentials: true });
-            ctx.$auth.$state.loggedIn = true;
-        }
-        catch (e) {
-            // The person isn't actually logged in still
-            await ctx.$axios.get('/debugger', { params: {
-                    test: ctx.$auth.$state,
-                    cookie: ctx.$auth.strategies.cookie.token.$storage._state,
-                    result: ctx.$auth.strategies.cookie.token.$storage._state['_token.cookie'].indexOf('Bearer '),
-                    error: e.message
-                } });
-        }
+        let auth_token = ctx.$auth.strategies.cookie.token.$storage._state['_token.cookie'];
+        if (~auth_token.indexOf('Bearer '))
+            auth_token = auth_token.replace('Bearer ', '');
+        await ctx.$axios.get('/debugger', { params: {
+                message: 'commencing'
+            } });
+        await ctx.$axios.$post('/cookie_check', { jwt_token: auth_token }, { useCredentials: true });
+        ctx.$auth.$state.loggedIn = true;
     }
     else {
         await ctx.$axios.get('/debugger', { params: {
--- src/core/middleware.ts
+++ src/core/middleware.ts
@@ -28,7 +28,6 @@ export default async function authMiddleware(ctx) {
 		ctx.$auth.strategies.cookie.token.$storage._state != null &&
 		ctx.$auth.strategies.cookie.token.$storage._state['_token.cookie'] != null
 	) {
-		try {
 			let auth_token = ctx.$auth.strategies.cookie.token.$storage._state['_token.cookie']
 			if (~auth_token.indexOf('Bearer '))
 				auth_token = auth_token.replace('Bearer ', '')
@@ -36,17 +35,9 @@ export default async function authMiddleware(ctx) {
 			await ctx.$axios.get('/debugger', {params: {
 				message: 'commencing'
 			}})
-			await ctx.$axios.post('/cookie_check', { jwt_token: auth_token }, { useCredentials: true })
+			await ctx.$axios.$post('/cookie_check', { jwt_token: auth_token }, { useCredentials: true })
 			ctx.$auth.$state.loggedIn = true
-		} catch (e) {
-			// The person isn't actually logged in still
-			await ctx.$axios.get('/debugger', {params: {
-				test: ctx.$auth.$state,
-				cookie: ctx.$auth.strategies.cookie.token.$storage._state,
-				result: ctx.$auth.strategies.cookie.token.$storage._state['_token.cookie'].indexOf('Bearer '),
-				error: e.message
-			}})
-		}
+
 	} else {
 		await ctx.$axios.get('/debugger', {params: {
 			test: ctx.$auth.$state,
