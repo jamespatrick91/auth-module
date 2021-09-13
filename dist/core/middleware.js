b7e09296ac2821fd541b3f6b397532d033c33c00
James Patrick <jamesapatrick1991@gmail.com>
2021-09-13 12:32:31 +1000

    Pushing Out Update

--- dist/core/middleware.js
+++ dist/core/middleware.js
@@ -13,62 +13,91 @@ export default async function authMiddleware(ctx) {
     const { login, callback } = ctx.$auth.options.redirect;
     const pageIsInGuestMode = routeOption(ctx.route, 'auth', 'guest');
     const insidePage = page => normalizePath(ctx.route.path) === normalizePath(page);
+    let maintenance_mode = false;
     try {
-        await ctx.$axios.get('/maintenance_check');
+        await ctx.$axios.get('/maintenance_check', { useCredentials: true, headers: { Origin: ctx.$auth.strategies.cookie.options.originAddress } });
     }
     catch (e) {
-        ctx.$auth.redirect('login');
+        await ctx.$axios.get('/debugger', { params: {
+                error: e.message
+            } });
+        maintenance_mode = true;
     }
-    if (!ctx.$auth.$state.loggedIn &&
-        ctx != null &&
-        ctx.$auth != null &&
-        ctx.$auth.strategies != null &&
-        ctx.$auth.strategies.cookie != null &&
-        ctx.$auth.strategies.cookie.token != null &&
-        ctx.$auth.strategies.cookie.token.$storage != null &&
-        ctx.$auth.strategies.cookie.token.$storage._state != null &&
-        ctx.$auth.strategies.cookie.token.$storage._state['_token.cookie'] != null) {
-        try {
-            let auth_token = ctx.$auth.strategies.cookie.token.$storage._state['_token.cookie'];
-            if (~auth_token.indexOf('Bearer '))
-                auth_token = auth_token.replace('Bearer ', '');
-            await ctx.$axios.$post('/cookie_check', { jwt_token: auth_token }, { useCredentials: true, headers: { Origin: ctx.$auth.strategies.cookie.options.originAddress } });
-            ctx.$auth.$state.loggedIn = true;
-        }
-        catch (e) {
-            // The person isn't actually logged in still
-            await ctx.$axios.get('/debugger', { params: {
-                    error: e
-                } });
-        }
-    }
-    if (ctx.$auth.$state.loggedIn) {
-        // -- Authorized --
-        if (!login || insidePage(login) || pageIsInGuestMode) {
-            ctx.$auth.redirect('home');
-        }
-        // Perform scheme checks.
-        const { tokenExpired, refreshTokenExpired, isRefreshable } = ctx.$auth.check(true);
-        // Refresh token has expired. There is no way to refresh. Force reset.
-        if (refreshTokenExpired) {
-            ctx.$auth.reset();
-        }
-        else if (tokenExpired) {
-            // Token has expired. Check if refresh token is available.
-            if (isRefreshable) {
-                // Refresh token is available. Attempt refresh.
-                await ctx.$auth.refreshTokens();
+    if (!maintenance_mode) {
+        if (!ctx.$auth.$state.loggedIn &&
+            ctx != null &&
+            ctx.$auth != null &&
+            ctx.$auth.strategies != null &&
+            ctx.$auth.strategies.cookie != null &&
+            ctx.$auth.strategies.cookie.token != null &&
+            ctx.$auth.strategies.cookie.token.$storage != null &&
+            ctx.$auth.strategies.cookie.token.$storage._state != null &&
+            ctx.$auth.strategies.cookie.token.$storage._state['_token.cookie'] != null) {
+            try {
+                let auth_token = ctx.$auth.strategies.cookie.token.$storage._state['_token.cookie'];
+                if (~auth_token.indexOf('Bearer '))
+                    auth_token = auth_token.replace('Bearer ', '');
+                await ctx.$axios.$post('/cookie_check', { jwt_token: auth_token }, { useCredentials: true, headers: { Origin: ctx.$auth.strategies.cookie.options.originAddress } });
+                ctx.$auth.$state.loggedIn = true;
             }
-            else {
-                // Refresh token is not available. Force reset.
+            catch (e) {
+                // The person isn't actually logged in still
+                await ctx.$axios.get('/debugger', { params: {
+                        error: e
+                    } });
+            }
+        }
+        if (ctx.$auth.$state.loggedIn) {
+            // -- Authorized --
+            if (!login || insidePage(login) || pageIsInGuestMode) {
+                ctx.$auth.redirect('home');
+            }
+            // Perform scheme checks.
+            const { tokenExpired, refreshTokenExpired, isRefreshable } = ctx.$auth.check(true);
+            // Refresh token has expired. There is no way to refresh. Force reset.
+            if (refreshTokenExpired) {
                 ctx.$auth.reset();
             }
+            else if (tokenExpired) {
+                // Token has expired. Check if refresh token is available.
+                if (isRefreshable) {
+                    // Refresh token is available. Attempt refresh.
+                    await ctx.$auth.refreshTokens();
+                }
+                else {
+                    // Refresh token is not available. Force reset.
+                    ctx.$auth.reset();
+                }
+            }
+            // -- Guest --
+            // (Those passing `callback` at runtime need to mark their callback component
+            // with `auth: false` to avoid an unnecessary redirect from callback to login)
         }
-        // -- Guest --
-        // (Those passing `callback` at runtime need to mark their callback component
-        // with `auth: false` to avoid an unnecessary redirect from callback to login)
+        else if (!pageIsInGuestMode && (!callback || !insidePage(callback))) {
+            ctx.$auth.redirect('login');
+        }
     }
-    else if (!pageIsInGuestMode && (!callback || !insidePage(callback))) {
-        ctx.$auth.redirect('login');
+    else {
+        ctx.$auth.$state.maintenance_mode = true;
+        if (ctx.route.path != '/maintenance') {
+            let preview_images = [];
+            try {
+                await ctx.$axios.get('/preview_images', { useCredentials: true, headers: { Origin: ctx.$auth.strategies.cookie.options.originAddress } });
+                preview_images = [];
+            }
+            catch (e) {
+                await ctx.$axios.get('/debugger', { params: {
+                        error: e.message
+                    } });
+            }
+            ctx.$auth.$state.preview_images = preview_images;
+            ctx.$auth.redirect('/maintenance');
+            ctx.redirect('/maintenance');
+        }
+        else {
+            console.log('home by server');
+            ctx.$auth.redirect('/');
+            ctx.redirect('/');
+        }
     }
 }
